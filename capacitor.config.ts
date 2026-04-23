d89d9c9ff1116b168ef3fb7753efe631e4d2e342
Russell Bedford <liquidlavaz@gmail.com>
2026-04-22 22:53:04 -0400

    Add Capacitor + GitHub Actions APK

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lavazlife.lavanotes',
+  appName: 'Lava Notes',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
